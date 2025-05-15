ecd85e9e6198d8f1fd2622cc52a8d8bdec04417c
lucas <lucaseschultz@gmail.com>
2025-05-15 12:20:34 -0600

    Converted skills-carousel to skills-cards

---
+++ resume/ui/components/skills-cards.js
@@ -0,0 +1,51 @@
+class SkillsCards extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="w3-row-padding transition" id="skills">
+        <div class='w3-padding-32'>
+          <h3 class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16" style="width: 4ch;">Skills</h3>
+        </div>
+          
+        <div class="skills-categories">
+          <div class="skill-category">
+            <h4>Frontend</h4>
+            <div class="skill-items">
+              <div class="skill-item"><img src="../images/html-icon.png" alt="HTML Logo"/> HTML/CSS</div>
+              <div class="skill-item"><img src="../images/javascript-icon.png" alt="JavaScript Logo"/> JavaScript/TypeScript</div>
+              <div class="skill-item"><img src="../images/react-icon.png" alt="React Logo"/> React/Next.js</div>
+              <div class="skill-item"><img src="../images/angular-icon.png" alt="Angular Logo"/> Angular</div>
+              <div class="skill-item"><img src="../images/tailwind-icon.png" alt="Tailwind Logo"/> CSS Frameworks</div>
+            </div>
+          </div>
+          
+          <div class="skill-category">
+            <h4>Backend</h4>
+            <div class="skill-items">
+              <div class="skill-item"><img src="../images/nodejs-icon.png" alt="Node Logo"/> Node.js/Express</div>
+              <div class="skill-item"><img src="../images/python-icon.png" alt="Python Logo"/> Python</div>
+              <div class="skill-item"><img src="../images/sql-icon.png" alt="SQL Logo"/> SQL/NoSQL Databases</div>
+              <div class="skill-item"><img src="../images/php-icon.png" alt="PHP Logo"/> PHP</div>
+            </div>
+          </div>
+          
+          <div class="skill-category">
+            <h4>Tools & Practices</h4>
+            <div class="skill-items">
+              <div class="skill-item"><img src="../images/git-icon.png" alt="Git Logo"/> Git/GitHub</div>
+              <div class="skill-item"><img src="../images/ci_cd-icon.png" alt="CI/CD Icon"/> CI/CD</div>
+              <div class="skill-item"><img src="../images/agile_icon.png" alt="Agile Logo"/> Agile</div>
+              <div class="skill-item"><img src="../images/ui_ux-icon.png" alt="UI/UX Icon"/> UI/UX Design</div>
+              <div class="skill-item"><img src="../images/openai-icon.png" alt="OpenAI Logo"/> AI Integration</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('skills-cards', SkillsCards);
