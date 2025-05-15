479c7189eed4174603fd1f571354969237296cc6
lucas <lucaseschultz@gmail.com>
2025-05-15 14:31:38 -0600

    Converted resume list to cards with details on technologies

--- resume/ui/components/resume-portfolio.js
+++ resume/ui/components/resume-portfolio.js
@@ -8,34 +8,39 @@ class ResumePortfolio extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="w3-row-padding transition" id="portfolio">
-          <div class='w3-padding-32'>
-              <h3
-                      class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16"
-                      style="width: 18ch;">
-                  Programming Portfolio</h3>
+        <div class='w3-padding-32'>
+          <h3 class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16" style="width: 18ch;">
+            Programming Portfolio
+          </h3>
+        </div>
+        <div class="portfolio-grid">
+          <div class="portfolio-item">
+            <h4>Palindrome Checker</h4>
+            <p>JavaScript app for efficient palindrome validation</p>
+            <p><strong>Tech:</strong> JavaScript, HTML, CSS</p>
+            <a href="https://wyomingschultz.com/palindrome-checker/" target="_blank" class="portfolio-link">
+              View Project <i class="fa fa-external-link"></i>
+            </a>
           </div>
-          <div>
-              <ul>
-                  <li>
-                      <a
-                              href="https://wyomingschultz.com/palindrome-checker/" target="_blank">
-                          Palindrome Checker <i class="fa fa-external-link"></i>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="https://wyomingschultz.com/roman-numeral-calculator/" target="_blank">
-                          Roman Numeral Calculator <i class="fa fa-external-link"></i>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="https://wyomingschultz.com/phone-number-validator/" target="_blank">
-                          Phone Number Validator <i class="fa fa-external-link"></i>
-                      </a>
-                  </li>
-              </ul>
+          <div class="portfolio-item">
+            <h4>Roman Numeral Calculator</h4>
+            <p>Interactive decimal/Roman numeral converter</p>
+            <p><strong>Tech:</strong> JavaScript, HTML, CSS</p>
+            <a href="https://wyomingschultz.com/roman-numeral-calculator/" target="_blank" class="portfolio-link">
+              View Project <i class="fa fa-external-link"></i>
+            </a>
           </div>
+          <div class="portfolio-item">
+            <h4>Phone Number Validator</h4>
+            <p>Regex-based phone format validation tool</p>
+            <p><strong>Tech:</strong> JavaScript, HTML, CSS, RegEx</p>
+            <a href="https://wyomingschultz.com/phone-number-validator/" target="_blank" class="portfolio-link">
+              View Project <i class="fa fa-external-link"></i>
+            </a>
+          </div>
+        </div>
       </div>
-      `;
+    `;
   }
 }
 
