bdb0f1722a9bbef432355a26c40d914a803618b0
lucas <lucaseschultz@gmail.com>
2025-05-16 12:50:53 -0600

    Removed unneeded carousel animation

--- resume/src/animations.ts
+++ resume/src/animations.ts
@@ -1,6 +1,4 @@
 const TRANSITION_ITEMS = document.querySelectorAll('.transition');
-const OBSERVABLE_CAROUSEL = document.querySelector('.carousel-slide')!;
-const CAROUSELS = document.querySelectorAll('.carousel-slide');
 
 const TRANSITION_OBSERVER = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -9,18 +7,5 @@ const TRANSITION_OBSERVER = new IntersectionObserver((entries) => {
         }
     })
 });
-const CAROUSEL_OBSERVER = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-            CAROUSELS.forEach((carouselElement) => {
-                carouselElement.classList.remove('pause');
-            })
-        }
-        else {
-            CAROUSELS.forEach((carouselElement) => {
-                carouselElement.classList.add('pause');
-            })
-        }
-})
 
-CAROUSEL_OBSERVER.observe(OBSERVABLE_CAROUSEL);
 TRANSITION_ITEMS.forEach((el) => TRANSITION_OBSERVER.observe(el));
