07c01f17b953e1bc025be9fd970d318b47855de9
lucas <lucaseschultz@gmail.com>
2025-05-19 08:41:31 -0600

    Changed experience-history.js's padding for consistent spacing across components

--- resume/ui/components/experience-history.js
+++ resume/ui/components/experience-history.js
@@ -8,7 +8,7 @@ class ExperienceHistory extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="w3-row w3-container" id="experience">
-            <div class="w3-padding-64 transition">
+            <div class="w3-padding-32 transition">
                 <h3
                         class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16" style="width: 9ch;">
                     Experience</h3>
