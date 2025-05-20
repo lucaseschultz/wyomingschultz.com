d484f21ed5311b4b72af3cc5a7a7a34b3e928af9
lucas <lucaseschultz@gmail.com>
2025-05-20 14:56:49 -0600

    Refactored education-history.js to be more visually appealing for hirers

--- resume/ui/components/education-history.js
+++ resume/ui/components/education-history.js
@@ -8,47 +8,67 @@ class EducationHistory extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="w3-row-padding transition" id="education">
-          <div class="w3-padding-32">
-              <h3
-                      class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16"
-                      style="width: 8ch;">Education</h3>
-          </div>
-          <div>
-              <ul class="education-list">
-                  <li>
-                      <a href='https://www.freecodecamp.org/learn/front-end-development-libraries/' target="_blank">
-                          Front End Development Libraries Certification <i class="fa fa-external-link"></i>
-                      </a>
-                      <span>Completed 2024</span>
-                  </li>
-                  <li>
-                      <a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/"
-                         target="_blank">
-                          JavaScript Algorithms and Data Structures Certification <i class="fa fa-external-link"></i>
-                      </a>
-                      <span>Completed 2024</span>
-                  </li>
-                  <li style="margin-bottom: .3rem;">
-                      <a href="https://internetingishard.netlify.app/html-and-css/" target="_blank">
-                          HTML & CSS Is Hard - Web Dev Tutorial <i class="fa fa-external-link"></i>
-                      </a>
-                      <span>Completed 2024</span>
-                  </li>
-                  <li style="margin-bottom: .3rem;">
-                      <a href="https://replit.com/learn/100-days-of-python/hub" target="_blank">
-                          100 Days of Code - The Complete Python Course <i class="fa fa-external-link"></i>
-                      </a>
-                      <span>Completed 2023</span>
-                  </li>
-                  <li style="margin-bottom: .3rem;">
-                      <b>Associates Degree in Criminal Justice</b>:
-                      <span>Rock Valley College, Rockford, Il</span>
-                      <span>Earned 2021</span>
-                  </li>
-              </ul>
-          </div>
+        <div class="w3-padding-32">
+          <h3 class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16" style="width: 8ch;">Education</h3>
+        </div>
+        <div>
+          <ul class="education-list">
+            <li class="education-item">
+              <div class="education-header">
+                <h4>Technical Certifications</h4>
+                <span class="education-date">2023-2024</span>
+              </div>
+              <div class="education-details">
+                <p><a href='https://www.freecodecamp.org/learn/front-end-development-libraries/' target="_blank">
+                  Front End Development Libraries Certification <i class="fa fa-external-link"></i>
+                </a></p>
+                <p class="education-description">
+                  React, Redux, Bootstrap, jQuery, SASS. Completed 5 projects demonstrating framework proficiency.
+                </p>
+                
+                <p><a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" target="_blank">
+                  JavaScript Algorithms and Data Structures Certification <i class="fa fa-external-link"></i>
+                </a></p>
+                <p class="education-description">
+                  Advanced JavaScript, algorithmic problem-solving, functional programming, OOP design patterns.
+                </p>
+                
+                <p><a href="https://replit.com/learn/100-days-of-python/hub" target="_blank">
+                  100 Days of Code - The Complete Python Course <i class="fa fa-external-link"></i>
+                </a></p>
+                <p class="education-description">
+                  Python data structures, algorithms, web development, and automation. Built web scrapers, data analysis tools, and APIs.
+                </p>
+              </div>
+            </li>
+            
+            <li class="education-item">
+              <div class="education-header">
+                <h4>Associates Degree in Criminal Justice</h4>
+                <span class="education-date">2021</span>
+              </div>
+              <div class="education-details">
+                <p>Rock Valley College, Rockford, IL</p>
+                <p class="education-description">
+                  Developed critical thinking and analytical skills that transferred to software engineering.
+                </p>
+              </div>
+            </li>
+            
+            <li class="education-item">
+              <div class="education-header">
+                <h4>Ongoing Professional Development</h4>
+              </div>
+              <div class="education-details">
+                <ul>
+                  <li>Building full-stack applications with modern frameworks</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      `;
+    `;
   }
 }
 
