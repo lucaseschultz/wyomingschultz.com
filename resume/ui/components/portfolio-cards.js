ff54405918b76998ba614f7ac29861e7a2f8cc32
lucas <lucaseschultz@gmail.com>
2025-05-21 09:28:53 -0600

    made portfolio cards clickable, moving link icon to top right of card, rather than having like at bottom of card

--- resume/ui/components/portfolio-cards.js
+++ resume/ui/components/portfolio-cards.js
@@ -14,29 +14,29 @@ class PortfolioCards extends HTMLElement {
           </h3>
         </div>
         <div class="portfolio-grid">
-          <div class="portfolio-item">
-            <h4>Palindrome Checker</h4>
-            <p>JavaScript app for efficient palindrome validation</p>
-            <p><strong>Tech:</strong> JavaScript, HTML, CSS</p>
-            <a href="https://wyomingschultz.com/palindrome-checker/" target="_blank" class="portfolio-link">
-              View Project <i class="fa fa-external-link"></i>
-            </a>
+          <div class="portfolio-item" onclick="window.open('https://wyomingschultz.com/palindrome-checker/', '_blank')">
+            <i class="fa fa-external-link portfolio-link-icon"></i>
+            <div class="portfolio-content">
+              <h4>Palindrome Checker</h4>
+              <p>JavaScript app for efficient palindrome validation</p>
+              <p><strong>Tech:</strong> JavaScript, HTML, CSS</p>
+            </div>
           </div>
-          <div class="portfolio-item">
-            <h4>Roman Numeral Calculator</h4>
-            <p>Interactive decimal/Roman numeral converter</p>
-            <p><strong>Tech:</strong> JavaScript, HTML, CSS</p>
-            <a href="https://wyomingschultz.com/roman-numeral-calculator/" target="_blank" class="portfolio-link">
-              View Project <i class="fa fa-external-link"></i>
-            </a>
+          <div class="portfolio-item" onclick="window.open('https://wyomingschultz.com/roman-numeral-calculator/', '_blank')">
+            <i class="fa fa-external-link portfolio-link-icon"></i>
+            <div class="portfolio-content">
+              <h4>Roman Numeral Calculator</h4>
+              <p>Interactive decimal/Roman numeral converter</p>
+              <p><strong>Tech:</strong> JavaScript, HTML, CSS</p>
+            </div>
           </div>
-          <div class="portfolio-item">
-            <h4>Phone Number Validator</h4>
-            <p>Regex-based phone format validation tool</p>
-            <p><strong>Tech:</strong> JavaScript, HTML, CSS, RegEx</p>
-            <a href="https://wyomingschultz.com/phone-number-validator/" target="_blank" class="portfolio-link">
-              View Project <i class="fa fa-external-link"></i>
-            </a>
+          <div class="portfolio-item" onclick="window.open('https://wyomingschultz.com/phone-number-validator/', '_blank')">
+            <i class="fa fa-external-link portfolio-link-icon"></i>
+            <div class="portfolio-content">
+              <h4>Phone Number Validator</h4>
+              <p>Regex-based phone format validation tool</p>
+              <p><strong>Tech:</strong> JavaScript, HTML, CSS, RegEx</p>
+            </div>
           </div>
         </div>
       </div>
