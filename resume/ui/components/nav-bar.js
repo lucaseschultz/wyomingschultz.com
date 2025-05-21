db481b278c0bfe6f25387eaa233625a45c8aae64
lucas <lucaseschultz@gmail.com>
2025-05-21 12:13:53 -0600

    refactored nav-bar class to be specific

--- resume/ui/components/nav-bar.js
+++ resume/ui/components/nav-bar.js
@@ -9,22 +9,22 @@ class NavBar extends HTMLElement {
     this.innerHTML = `
     <nav class="nav-bar-container">
         <div class="nav-bar w3-row w3-large w3-light-grey header">
-            <div class="w3-col">
+            <div class="nav-bar-button-container">
                 <a role="button" href="#about" class="nav-bar-button">About</a>
             </div>
-            <div class="w3-col">
+            <div class="nav-bar-button-container">
                 <a role="button" href="#skills" class="nav-bar-button">Skills</a>
             </div>
-            <div class="w3-col">
+            <div class="nav-bar-button-container">
                 <a role="button" href="#portfolio" class="nav-bar-button">Portfolio</a>
             </div>
-            <div class="w3-col">
+            <div class="nav-bar-button-container">
                 <a role="button" href="#education" class="nav-bar-button">Education</a>
             </div>
-            <div class="w3-col header-last-row">
+            <div class="nav-bar-button-container">
                 <a role="button" href="#experience" class="nav-bar-button">Experience</a>
             </div>
-            <div class="w3-col header-last-row">
+            <div class="nav-bar-button-container">
                 <a role="button" href="#contact" class="nav-bar-button">Contact</a>
             </div>
         </div>
