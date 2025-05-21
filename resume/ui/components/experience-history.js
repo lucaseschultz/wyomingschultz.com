7dce955f62f376e959388bfae719c7da1c1b8e57
lucas <lucaseschultz@gmail.com>
2025-05-21 14:58:56 -0600

    removed unused class

--- resume/ui/components/experience-history.js
+++ resume/ui/components/experience-history.js
@@ -7,7 +7,7 @@ class ExperienceHistory extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <div class="w3-row w3-container" id="experience">
+      <div class="w3-row" id="experience">
             <div class="w3-padding-32 transition">
                 <h3
                         class="component-header w3-xlarge" style="width: 9ch;">
