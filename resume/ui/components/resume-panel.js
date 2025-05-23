55ad8837b30a22b3c44c6e8fec19bdc5da779831
lucas <lucaseschultz@gmail.com>
2025-05-23 09:19:15 -0600

    renamed .w3-panel to be panel

--- resume/ui/components/resume-panel.js
+++ resume/ui/components/resume-panel.js
@@ -7,7 +7,7 @@ class ResumePanel extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-        <div class="w3-panel">
+        <div class="panel">
             <h1 id="name"><b>LUCAS E. SCHULTZ</b></h1>
             <img class="profile" src="../images/lucas-schultz.png" alt="" />
             <p></p>
